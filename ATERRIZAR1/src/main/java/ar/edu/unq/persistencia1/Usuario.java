62e4b226bfeba98251eaa39097a5dabfc683550e
lalo <leandro.gz73@gmail.com>
2014-03-27 08:54:19 -0300

    Add user class

---
+++ ATERRIZAR1/src/main/java/ar/edu/unq/persistencia1/Usuario.java
@@ -0,0 +1,17 @@
+package ar.edu.unq.persistencia1;
+
+import java.util.Date;
+
+public class Usuario {
+    String nombre, apellido, nombreDeUsuario, email;
+    Date birthday;
+
+    public Usuario(String nombre, String apellido, String nombreDeUsuario, String email, Date birthday) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nombreDeUsuario = nombreDeUsuario;
+        this.email = email;
+        this.birthday = birthday;
+    }
+
+}
