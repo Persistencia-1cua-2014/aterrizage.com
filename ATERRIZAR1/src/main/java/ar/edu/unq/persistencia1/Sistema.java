8cbc41d74b9537f55473f4cef9425bebcded1e4a
lalo <leandro.gz73@gmail.com>
2014-03-27 12:24:34 -0300

    add test sistema

--- ATERRIZAR1/src/main/java/ar/edu/unq/persistencia1/Sistema.java
+++ ATERRIZAR1/src/main/java/ar/edu/unq/persistencia1/Sistema.java
@@ -2,6 +2,7 @@ package ar.edu.unq.persistencia1;
 
 
 import ar.edu.unq.persistencia1.homes.RepositorioDeUsuarios;
+import ar.edu.unq.persistencia1.mailer.Mailer;
 
 public class Sistema {
 
@@ -11,11 +12,25 @@ public class Sistema {
 
         RepositorioDeUsuarios repo = this.getRepositorioDeUsuarios();
         repo.guardarUsuario(usuario);
+
+    }
+    public void sendMail(Usuario usuario){
+        String codigoDeValidacion = this.codigoDeValidacion(usuario);
+        Mailer mail = this.getMailer();
+        mail.sendEmail(usuario.getEmail(),codigoDeValidacion);
     }
 
     public RepositorioDeUsuarios getRepositorioDeUsuarios(){
         return new RepositorioDeUsuarios(databaseName);
 
+    }
+    public String codigoDeValidacion(Usuario usuario){
+        return usuario.getNombreDeUsuario();
+
+    }
+    public Mailer getMailer(){
+        Mailer mail = new Mailer();
+        return mail;
     }
 
 }
---
+++ ATERRIZAR1/src/test/java/sistemaTest/TestSistema.java
@@ -0,0 +1,28 @@
+package sistemaTest;
+
+import ar.edu.unq.persistencia1.Sistema;
+import ar.edu.unq.persistencia1.Usuario;
+import ar.edu.unq.persistencia1.mailer.Mailer;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.stub;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by lalo on 27/03/14.
+ */
+public class TestSistema{
+    @Test
+    public void testeSendEmail(){
+        Mailer mailer = mock(Mailer.class);
+        Sistema sistema = mock(Sistema.class);
+        stub(sistema.getMailer()).toReturn(mailer);
+        Usuario usuario = new Usuario("lalo", "landa", "laloCura", "unEmail", new Date());
+        sistema.sendMail(usuario);
+        verify(mailer).sendEmail(usuario.getEmail(), sistema.codigoDeValidacion(usuario));
+    }
+
+}
