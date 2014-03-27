8bbf5fbca477fe59f941cd7701f259cf3fac7130
lalo <leandro.gz73@gmail.com>
2014-03-27 09:39:54 -0300

    Add service

--- ATERRIZAR1/src/main/java/ar/edu/unq/persistencia1/services/Service.java
+++ ATERRIZAR1/src/main/java/ar/edu/unq/persistencia1/services/Service.java
@@ -1,7 +1,25 @@
 package ar.edu.unq.persistencia1.services;
 
-/**
- * Created by lalo on 27/03/14.
- */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Service {
+
+    String database;
+
+    public Service(String database) {
+        this.database = database;
+    }
+
+
+    public Connection getConnection() throws Exception {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost/" + this.database + "?user=root&password=root");
+    }
+
+    public void emptyTable(String table_name) throws Exception{
+        Connection connection = this.getConnection();
+        connection.prepareStatement("DELETE FROM " + this.database + "." + table_name).execute();
+    }
 }
