4a50391e6931e3dea72bde6038a134f824f5434b
lalo <leandro.gz73@gmail.com>
2014-03-27 11:36:15 -0300

    add integration between system and repository

--- ATERRIZAR1/src/main/java/ar/edu/unq/persistencia1/Sistema.java
+++ ATERRIZAR1/src/main/java/ar/edu/unq/persistencia1/Sistema.java
@@ -1,9 +1,20 @@
 package ar.edu.unq.persistencia1;
 
 
+import ar.edu.unq.persistencia1.homes.RepositorioDeUsuarios;
+
 public class Sistema {
 
-    public void registrarUsuario(Usuario usuario) throws UsuarioYaExisteException {
+    static String databaseName = "aterrizage";
+
+    public void registrarUsuario(Usuario usuario) throws Exception {
+
+        RepositorioDeUsuarios repo = this.getRepositorioDeUsuarios();
+        repo.guardarUsuario(usuario);
+    }
+
+    public RepositorioDeUsuarios getRepositorioDeUsuarios(){
+        return new RepositorioDeUsuarios(databaseName);
 
     }
 
