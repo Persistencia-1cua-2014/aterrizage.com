6f4179975e429098ccf7f981077c7cc56226440b
lalo <leandro.gz73@gmail.com>
2014-03-27 08:27:06 -0300

    Add another print

--- ATERRIZAR1/src/main/java/ar/edu/unq/persistencia1/SimpleTest.java
+++ ATERRIZAR1/src/main/java/ar/edu/unq/persistencia1/SimpleTest.java
@@ -3,5 +3,6 @@ package ar.edu.unq.persistencia1;
 public class SimpleTest {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+        System.out.println("Hello world!");
 	}
 }
