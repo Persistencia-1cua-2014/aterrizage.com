ea538f20937f880c2649e00c279ca3fd2bb0a755
juliatroilo <juliatroilo@gmail.com>
2014-04-03 14:05:21 -0300

    Agregado xml

--- ATERRIZAR1/src/main/java/ar/edu/unq/persistencia1/Usuario.java
+++ ATERRIZAR1/src/main/java/ar/edu/unq/persistencia1/Usuario.java
@@ -3,19 +3,30 @@ package ar.edu.unq.persistencia1;
 import java.util.Date;
 
 public class Usuario {
-    String nombre, apellido, nombreDeUsuario, email, password;
+    String nombre, apellido, nombreDeUsuario, email, password, codigoDeValidacion;
     Date birthday;
+    Boolean verificado;
 
-    public Usuario(String nombre, String apellido, String nombreDeUsuario, String email, Date birthday,String pass) {
+    public Usuario(String nombre, String apellido, String nombreDeUsuario, String email, Date birthday,String pass, String codigoDeValidacion ) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.nombreDeUsuario = nombreDeUsuario;
         this.email = email;
         this.birthday = birthday;
+        this.verificado = false;
         this.password= pass;
+        this.codigoDeValidacion= codigoDeValidacion;
     }
 
-    public String getNombreDeUsuario() {
+    public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNombreDeUsuario() {
         return nombreDeUsuario;
     }
 
@@ -34,4 +45,20 @@ public class Usuario {
     public Date getBirthday() {
         return birthday;
     }
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
 }
