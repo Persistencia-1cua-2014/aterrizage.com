36f19ecf438c97d681251a3a67e7c275f039ceea
lalo <leandro.gz73@gmail.com>
2014-04-03 12:16:21 -0300

    refactor Sistema class methods

--- ATERRIZAR1/src/main/java/ar/edu/unq/persistencia1/Sistema.java
+++ ATERRIZAR1/src/main/java/ar/edu/unq/persistencia1/Sistema.java
@@ -15,36 +15,15 @@ public class Sistema {
         repo.guardarUsuario(usuario);
 
     }
-    public void sendMail(Usuario usuario)throws Exception{
-        String codigoDeValidacion = this.codigoDeValidacion(usuario);
-        Mailer mail = this.getMailer();
-        mail.sendEmail(usuario.getEmail(),codigoDeValidacion);
+
+    public void validarCuenta(String codigo) throws Exception {
+        RepositorioDeUsuarios repo = this.getRepositorioDeUsuarios();
+        repo.validarCuenta(codigo);
     }
 
-    public RepositorioDeUsuarios getRepositorioDeUsuarios(){
+    public RepositorioDeUsuarios getRepositorioDeUsuarios() {
         return new RepositorioDeUsuarios(databaseName);
 
-    }
-    public String codigoDeValidacion(Usuario usuario) throws Exception{
-        this.getRepositorioDeUsuarios().guardarCodigo(usuario,usuario.getNombreDeUsuario());
-        return usuario.getNombreDeUsuario();
-
-    }
-    public Mailer getMailer(){
-        Mailer mail = new Mailer();
-        return mail;
-    }
-
-
-   public void validarCuenta(String codigo) throws Exception{
-	   RepositorioDeUsuarios repo = this.getRepositorioDeUsuarios();
-	   repo.validarCuenta(codigo);
-
-
-   }
-
-   public boolean existeCodigo(String codigo) throws Exception{
-       return this.getRepositorioDeUsuarios().existeCodigo(codigo);
     }
 
     public Usuario ingresarUsuario(String nombreDeUsuario, String password) throws Exception {
@@ -52,19 +31,39 @@ public class Sistema {
         return user;
     }
 
-   
-   
-   
-  public void cambiarPassword(String userName,String pass,String nuevaPass) throws Exception{
-	  if(this.getRepositorioDeUsuarios().existeUsuarioConPass(userName,pass) && pass.equals(nuevaPass)){ 
-	  
-		  this.getRepositorioDeUsuarios().cambiarPassword(nuevaPass);
-	  }
-	  else{
-		  throw new NuevaPasswordInvalida();
-	  }
-   
-  }
-   
+    public void cambiarPassword(String userName, String pass, String nuevaPass) throws Exception {
+        if (this.getRepositorioDeUsuarios().existeUsuarioConPass(userName, pass) && pass.equals(nuevaPass)) {
+
+            this.getRepositorioDeUsuarios().cambiarPassword(nuevaPass);
+        } else {
+            throw new NuevaPasswordInvalida();
+        }
+
+    }
+
+
+    // help methods
+
+    public String codigoDeValidacion(Usuario usuario) throws Exception {
+        this.getRepositorioDeUsuarios().guardarCodigo(usuario, usuario.getNombreDeUsuario());
+        return usuario.getNombreDeUsuario();
+
+    }
+
+    public Mailer getMailer() {
+        Mailer mail = new Mailer();
+        return mail;
+    }
+
+    public boolean existeCodigo(String codigo) throws Exception {
+        return this.getRepositorioDeUsuarios().existeCodigo(codigo);
+    }
+
+    public void sendMail(Usuario usuario) throws Exception {
+        String codigoDeValidacion = this.codigoDeValidacion(usuario);
+        Mailer mail = this.getMailer();
+        mail.sendEmail(usuario.getEmail(), codigoDeValidacion);
+    }
+
 
 }
