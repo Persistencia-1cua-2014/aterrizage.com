05e042945d436a832a96d677a27311d15156e46f
lalo <leandro.gz73@gmail.com>
2014-05-08 13:16:21 -0300

    Add Aerolinea mapping

--- ATERRIZAR1/src/main/java/ar/edu/unq/persistencia1/enterprise/Aerolinea.java
+++ ATERRIZAR1/src/main/java/ar/edu/unq/persistencia1/enterprise/Aerolinea.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Aerolinea {
     private List<Vuelo> vueloList;
+    private int idAerolinea;
 
     public Aerolinea() {
         setVueloList(new ArrayList<Vuelo>());
@@ -16,5 +17,13 @@ public class Aerolinea {
 
     public void setVueloList(List<Vuelo> vueloList) {
         this.vueloList = vueloList;
+    }
+
+    public int getIdAerolinea() {
+        return idAerolinea;
+    }
+
+    public void setIdAerolinea(int idAerolinea) {
+        this.idAerolinea = idAerolinea;
     }
 }
