c2be3b23232bacef8f855872a1ca90c9c8e9bcbc
lalo <leandro.gz73@gmail.com>
2014-07-03 12:44:17 -0300

    Add getFriend

--- ATERRIZAR1/src/main/java/ar/edu/unq/persistencia1/homes/SocialNetworkManager.java
+++ ATERRIZAR1/src/main/java/ar/edu/unq/persistencia1/homes/SocialNetworkManager.java
@@ -4,11 +4,16 @@ package ar.edu.unq.persistencia1.homes;
 import ar.edu.unq.persistencia1.Usuario;
 import ar.edu.unq.persistencia1.services.neo4j.NeoManager;
 import ar.edu.unq.persistencia1.services.neo4j.operations.AddFriend;
+import ar.edu.unq.persistencia1.services.neo4j.operations.GetFriends;
 
 public class SocialNetworkManager {
 
     public void addFriend(Usuario usuario, Usuario newFriend){
         NeoManager.runInSession(new AddFriend(usuario, newFriend));
+    }
+
+    public void getFriend(Usuario usuario){
+        NeoManager.runInSession(new GetFriends(usuario));
     }
 
 }
