3e631e8d4080d330084e8692dc80abea49bd46a8
Leandro Gomez <leandro.gz73@gmail.com>
2014-07-10 11:01:26 -0300

    write test

---
+++ ATERRIZAR1/src/main/java/ar/edu/unq/persistencia1/homes/CommentsManager.java
@@ -0,0 +1,12 @@
+package ar.edu.unq.persistencia1.homes;
+
+import ar.edu.unq.persistencia1.Usuario;
+import ar.edu.unq.persistencia1.enterprise.Lugar;
+
+public class CommentsManager {
+
+    public void addDestination(Usuario user, Lugar lugar){
+
+    }
+
+}
--- ATERRIZAR1/src/test/java/mongodb_integration/MongoIntegrationTest.java
+++ ATERRIZAR1/src/test/java/mongodb_integration/MongoIntegrationTest.java
@@ -1,5 +1,8 @@
 package mongodb_integration;
 
+import ar.edu.unq.persistencia1.Usuario;
+import ar.edu.unq.persistencia1.enterprise.Lugar;
+import ar.edu.unq.persistencia1.homes.CommentsManager;
 import ar.edu.unq.persistencia1.services.mongodb.MongoManager;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
@@ -7,8 +10,12 @@ import com.mongodb.DBCollection;
 import junit.framework.TestCase;
 
 import java.util.Date;
+import java.util.List;
 
 public class MongoIntegrationTest extends TestCase{
+
+    private Usuario usuario;
+    private Lugar lugar;
 
     public void testMongoInsert(){
         DB database = MongoManager.getDatabase("mongo_test");
@@ -19,8 +26,16 @@ public class MongoIntegrationTest extends TestCase{
         document.put("createdDate", new Date());
         table.insert(document);
 
+    }
 
+    public void testAddDestination(){
+        this.usuario = new Usuario("Lalocura", "DeLalo", "Lalo", "Laloooo", new Date(), "12", "");
+        this.lugar = new Lugar("Rio de Janeiro");
+        CommentsManager manager = new CommentsManager();
+        manager.addDestination(usuario, lugar);
 
+        List<Lugar> lugares =  manager.getDestinations(usuario);
+        assertEquals(1, lugares.size());
 
     }
 
